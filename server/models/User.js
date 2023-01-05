7aebc165083244f82a5296564f58d8bc45e522b0
Vaibhavihole31 <itspinki05@gmail.com>
2023-01-06 00:34:25 +0530

    feat: model validation

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,11 @@
 import {Schema, model} from "mongoose";
 
 const userSchema = new Schema({
-    name: String,
-    email: String,
-    phone: String,
-    password: String,
-    role: String
+    name: { type: String, require: [true, 'name cannot be empty'] },
+    email: { type: String, require: [true, 'email cannot be empty'] },
+    phone: { type: String, require: [true, 'phone cannot be empty'] },
+    password: { type: String, require: [true, 'password cannot be empty'] },
+    role: { type: String, require: [true, 'role cannot be empty'] },
 },
 {
     timestamps: true
