c4458983183e3c0f680177e209ae7879d33d4a14
Vaibhavihole31 <itspinki05@gmail.com>
2023-01-06 00:42:40 +0530

    feat: signup api

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config();
 
+import User from './models/User.js'
+
 const app = express();
 app.use(express.json());
 
@@ -14,6 +16,28 @@ mongoose.connect(process.env.MONGODB_URL, ()=>{
     console.log('Connected To MongoDB🤝');
 })
 
+
+// api route start here
+
+app.post('/signup', async (req, res) => {
+    const {name, phone, email, password, role} = req.body;
+
+    const user = new User({
+        name: name,
+        phone: phone,
+        email: email,
+        password: password,
+        role: role
+    })
+
+    const savedUser = await user.save();
+
+    res.json({
+        success: true,
+        message: "User Created Successfully",
+        data: savedUser
+    })
+})
 
 app.listen(PORT, ()=>{
     console.log(`server is running on ${PORT} ✈️`);
