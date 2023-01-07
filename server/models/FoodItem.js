779f8978523e1e6ffb1edffcdeca326993c04e4a
Vaibhavihole31 <itspinki05@gmail.com>
2023-01-07 15:46:49 +0530

    feat: validation for food item model

--- server/models/FoodItem.js
+++ server/models/FoodItem.js
@@ -1,11 +1,11 @@
-import {Schema, model} from 'mongoose';
+import { Schema, model } from 'mongoose';
 
 const foodItemSchema = new Schema({
-    title: String,
-    description : String,
-    imagUrl : String,
-    price : Number,
-    catergory : String
+    title: { type: String, require: [true, 'title cannot be empty'] },
+    description: { type: String, require: [true, 'description cannot be empty'] },
+    imagUrl: { type: String, require: [true, 'image url cannot be empty'] },
+    price: { type: Number, require: [true, 'price cannot be empty'] },,
+    catergory: { type: String, require: [true, 'catergory cannot be empty'] },
 })
 
 const FoodItem = model("FoodItem", foodItemSchema)
