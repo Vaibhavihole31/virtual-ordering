d059016d34b589f620daa18baca039347cc33e51
Vaibhavihole31 <itspinki05@gmail.com>
2023-01-08 11:14:23 +0530

    ref: validation for order model

--- server/models/Order.js
+++ server/models/Order.js
@@ -1,16 +1,16 @@
 import { Schema, model } from "mongoose";
 
 const orderSchema = new Schema({
-    orderId: String,
-    tableNumber : Number,
+    orderId: { type: String, require: [true, 'orderId cannot be empty'] },
+    tableNumber: { type: Number, require: [true, 'table Number cannot be empty'] },
     userId: {
         type: Schema.Types.ObjectId,
         ref: "User"
     },
     items: {
-        name: String,
-        price: Number,
-        quantity: Number
+        name: { type: String, require: [true, 'item name cannot be empty'] },
+        price: { type: Number, require: [true, 'price cannot be empty'] },
+        quantity: { type: Number, require: [true, 'quantity cannot be empty'] },
     }
 })
 
