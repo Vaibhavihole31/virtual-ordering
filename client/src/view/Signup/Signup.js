4584c99e068e7fcbd7ce7e5a16806bad0c3786f8
Vaibhavihole31 <itspinki05@gmail.com>
2023-01-11 22:48:47 +0530

    feat: frontend + backend intergration of signup page

--- client/src/view/Signup/Signup.js
+++ client/src/view/Signup/Signup.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Signup.css'
+import axios from 'axios';
 import Order from './order.png'
 
 function Signup() {
+
+        const [ name, setName ] = useState("");
+        const [ email, setEmail ] = useState("");
+        const [ phone, setPhone ] = useState("");
+        const [ password, setPassword ] = useState("");
+        const [role, setRole] = useState("user")
+
+
+        async function addUser() {
+            const response = await axios.post('/signup', {
+                name: name,
+                email: email,
+                password: password,
+                phone: phone,
+                role: role
+            })
+
+            console.log(response);
+        }
+
     return (
         <>
             <div className='container'>
                 <div className='row'>
-                    <div className='col-md-6'>
+                    <div className='col-md-5'>
                         <img className='order-img' src={Order} />
                     </div>
+                    <div className='col-md-2'></div>
 
-                    <div className='col-md-6 text-center'>
+                    <div className='col-md-5 text-center'>
                         <div className='signup-card'>
                             <h2 className='text-center'>Signup</h2>
 
@@ -22,6 +44,7 @@ function Signup() {
                                         className="form-control input-control"
                                         id="name"
                                         placeholder='name'
+                                        value={name} onChange={(e) => { setName(e.target.value) }}
                                     />
                                 </div>
                                 <div className="mb-3 mt-5" >
@@ -30,6 +53,7 @@ function Signup() {
                                         className="form-control input-control"
                                         id="email"
                                         placeholder='email'
+                                        value={email} onChange={(e) => { setEmail(e.target.value) }}
                                     />
                                 </div>
                                 <div className="mb-3 mt-5" >
@@ -38,6 +62,7 @@ function Signup() {
                                         className="form-control input-control"
                                         id="phone"
                                         placeholder='phone'
+                                        value={phone} onChange={(e) => { setPhone(e.target.value) }}
                                     />
                                 </div>
                                 <div className="mb-3 mt-5" >
@@ -46,9 +71,10 @@ function Signup() {
                                         className="form-control input-control"
                                         id="password"
                                         placeholder='password'
+                                        value={password} onChange={(e) => { setPassword(e.target.value) }}
                                     />
                                 </div>
-                                <button className="signup-page-btn w-100 mb-5" type="button">
+                                <button className="signup-page-btn w-100 mb-5" type="button" onClick={addUser}>
                                     <i class="fa-solid fa-right-to-bracket"></i> <b>Signup</b>
                                 </button>
                             </form>
