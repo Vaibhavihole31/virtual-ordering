64e5a1479c06ae139e44bd6ffa4aa3f021a2fa93
Vaibhavihole31 <itspinki05@gmail.com>
2023-01-11 21:52:31 +0530

    feat: sigup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Signup from './view/Signup/Signup'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ client/src/view/Signup/Signup.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import './Signup.css'
+import Order from './order.png'
+
+function Signup() {
+    return (
+        <>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-md-6'>
+                        <img className='order-img' src={Order} />
+                    </div>
+
+                    <div className='col-md-6 text-center'>
+                        <div className='signup-card'>
+                            <h2 className='text-center'>Signup</h2>
+
+                            <form>
+                                <div className="mb-3 mt-5" >
+                                    <input
+                                        type="text"
+                                        className="form-control input-control"
+                                        id="name"
+                                        placeholder='name'
+                                    />
+                                </div>
+                                <div className="mb-3 mt-5" >
+                                    <input
+                                        type="text"
+                                        className="form-control input-control"
+                                        id="email"
+                                        placeholder='email'
+                                    />
+                                </div>
+                                <div className="mb-3 mt-5" >
+                                    <input
+                                        type="text"
+                                        className="form-control input-control"
+                                        id="phone"
+                                        placeholder='phone'
+                                    />
+                                </div>
+                                <div className="mb-3 mt-5" >
+                                    <input
+                                        type="password"
+                                        className="form-control input-control"
+                                        id="password"
+                                        placeholder='password'
+                                    />
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Signup
