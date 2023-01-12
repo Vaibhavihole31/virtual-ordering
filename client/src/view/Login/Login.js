33e9b7e27656c3eb921b2e788d4acdc1d3c2fb82
Vaibhavihole31 <itspinki05@gmail.com>
2023-01-12 15:40:27 +0530

    feat: login page

--- client/src/view/Login/Login.js
+++ client/src/view/Login/Login.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React from 'react';
+import './Login.css';
+import LoginImg from './login-img.png';
 
 function Login() {
   return (
-    <div>Login</div>
+    <>
+      <div className='container'>
+        <div className='login-card'>
+          <div className='row'>
+            <h2 className='text-center'>Login</h2>
+
+            <div className='col-md-5'>
+              <img src={LoginImg} className="login-img"/>
+            </div>
+            <div className='col-md-7'>
+              <div className="mb-3 mt-5" >
+                <div className="mb-3 mt-5" >
+
+                  <input
+                    type="text"
+                    className="form-control input-control"
+                    id="email"
+                    placeholder='email'
+                  />
+                </div>
+                <input
+                  type="password"
+                  className="form-control input-control"
+                  id="password"
+                  placeholder='password'
+                />
+              </div>
+
+              <button className="signup-page-btn w-100 mb-5 btn btn-primary" type="button">
+                <i class="fa-solid fa-right-to-bracket"></i> <b>Signup</b>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
