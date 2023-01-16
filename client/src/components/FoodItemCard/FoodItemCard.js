65aeb798070cb879885b3905ec56561dc27d34d9
Vaibhavihole31 <itspinki05@gmail.com>
2023-01-16 22:50:06 +0530

    ref: card design

--- client/src/components/FoodItemCard/FoodItemCard.js
+++ client/src/components/FoodItemCard/FoodItemCard.js
@@ -1,45 +1,47 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './FoodItemCard.css'
 
 function FoodItemCard({ category, description, imgUrl, price, title }) {
 
   const [quantity, setQuantity] = useState(1)
 
-  async function addToList(){
-   const listObject = {
-     name: title,
-     price: price,
-     quantity: quantity
-   }
+  async function addToList() {
+    const listObject = {
+      name: title,
+      price: price,
+      quantity: quantity
+    }
 
-   const existingList = JSON.parse(localStorage.getItem('list')) || []
+    const existingList = JSON.parse(localStorage.getItem('list')) || []
 
-   existingList.push(listObject)
+    existingList.push(listObject)
 
-   localStorage.setItem('list', JSON.stringify(existingList))
+    localStorage.setItem('list', JSON.stringify(existingList))
   }
 
   return (
-    <div className='col-md-3'>
-      <div className="food-item-card mt-5">
-        <div>
-          <img src={imgUrl} class="food-item-card-header-img" />
-        </div>
-        <h3 className='text-center'>{title}</h3>
-        <p className='text-center'>{description || title}</p>
-        <div className='price-btn-container'>
-        <b>{price}/- Only</b>
-        <b>{category}</b>
-        </div>
+    <div className="food-item-card mb-5">
+      <div className="row">
+        <div className="col-md-12">
+          <div>
+            <img src={imgUrl} class="food-item-card-header-img" />
+          </div>
 
-        <div className='quantity-btn-container'>
-          <span className='qnt-btn' onClick={(e)=>{setQuantity(quantity-1)}}><i class="fa-sharp fa-solid fa-minus"></i></span>
-          <span className='qnt-text'>{quantity}</span>
-          <span className='qnt-btn' onClick={(e)=>{setQuantity(quantity+1)}}><i class="fa-solid fa-plus"></i></span>
-        </div>
+          <h3 className='text-center'>{title}</h3>
+          <p className='text-center'>{description || title}</p>
+          <div className='price-btn-container'>
+            <b>{price}/- Only</b>
+            <b>{category}</b>
+          </div>
+          <div className='quantity-btn-container'>
+            <span className='qnt-btn' onClick={(e) => { setQuantity(quantity - 1) }}><i class="fa-sharp fa-solid fa-minus"></i></span>
+            <span className='qnt-text'>{quantity}</span>
+            <span className='qnt-btn' onClick={(e) => { setQuantity(quantity + 1) }}><i class="fa-solid fa-plus"></i></span>
+          </div>
 
-        <div>
-          <button type="button" className='text-center btn-add-to-list' onClick={addToList}><b><i class="fa-solid fa-cart-plus"></i> Add To List</b></button>
+          <div>
+            <button type="button" className='text-center btn-add-to-list' onClick={addToList}><b><i class="fa-solid fa-cart-plus"></i> Add To List</b></button>
+          </div>
         </div>
       </div>
     </div>
