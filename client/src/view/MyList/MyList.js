9f1d5d290635f00141dda9097b1432e234421bd0
Vaibhavihole31 <itspinki05@gmail.com>
2023-01-22 13:03:30 +0530

    feat: order card

--- client/src/view/MyList/MyList.js
+++ client/src/view/MyList/MyList.js
@@ -1,31 +1,30 @@
 import React from 'react'
 import './MyList.css'
-import {myFoodListItems} from './../../utils/myList';
-import FoodItemCard from '../../components/FoodItemCard/FoodItemCard';
+import { myFoodListItems } from './../../utils/myList';
 
 function MyList() {
-  return (
-   <>
-    <div>
-        <h1 className='text-center heading' >🍟 MyList</h1>
-        {
-            myFoodListItems.map((item, index)=>{
-                return(
-                    <div>
-                        <h6>Name: {item.name}</h6>
-                        <h6>Quantity: {item.quantity}</h6>
-                        <h6>Price: {item.price}</h6>
-                        <hr/>
-                    </div>
-                ) 
-            })
-        }
-       <div className='text-center'>
-       <button className='btn btn-primary confirm-btn'><b>Confirm Orders</b> <i class="fa-solid fa-circle-check"></i></button>
-       </div>
-    </div>
-   </>
-  )
+    return (
+        <>
+            <div>
+                <h1 className='text-center heading mb-5'>🍟 MyList</h1>
+                {
+                    myFoodListItems.map((item, index) => {
+                        return (
+                            <div className='subject-card'>
+                                <h4>{item.name}</h4>
+                                <h6>Quantity: {item.quantity}</h6>
+                                <h6 className='mb-2'>₹:{item.price}</h6>
+                                <img className='order-img' src={item.imgUrl}/>
+                            </div>
+                        )
+                    })
+                }
+                <div className='text-center'>
+                    <button className='btn btn-primary confirm-btn'><b>Confirm Orders</b> <i class="fa-solid fa-circle-check"></i></button>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default MyList
