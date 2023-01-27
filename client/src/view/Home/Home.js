f5c0c6f71b589a46bd3ee25a716577628188b66d
Vaibhavihole31 <itspinki05@gmail.com>
2023-01-27 22:10:56 +0530

    feat: after clicking button redirect to flavour feat

--- client/src/view/Home/Home.js
+++ client/src/view/Home/Home.js
@@ -1,5 +1,5 @@
-
 import homePageImg from './home-page-icecream.jpg'
+import { Link } from 'react-router-dom'
 import './Home.css'
 
 function Home() {
@@ -14,12 +14,14 @@ function Home() {
              <h4 className='text-center mt-5'>And so are all the delicious recipes that contain it. Start here with the Perfect Affogato!</h4>
              
              <div className='text-center mt-5'>
+             <Link to="/floveritem">
              <button className="flavor-btn text-center"><b>Try Our Flavor 🍧</b></button>
+             </Link>
              </div>
           </div>
           <div className='col-md-6'>
             <div> 
-              <img className='home-page-img' src={homePageImg}/>
+              <img className='home-page-img' src={homePageImg} />
             </div>
           </div>
         </div>
