2eb38d15c526f6311fb8e5975bf6c3cd68d4feae
Vaibhavihole31 <itspinki05@gmail.com>
2023-01-27 22:03:19 +0530

    feat: all food item fetch in flover item view

--- client/src/view/FlavorItem/FlavorItem.js
+++ client/src/view/FlavorItem/FlavorItem.js
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import FoodItemCard from '../../components/FoodItemCard/FoodItemCard';
+import { loginRequired } from '../../utils/loginRequired';
 
 function FlavorItem() {
+
+    
+  const [searchText, setSearchText] = useState("");
+  const [currentFoodItems, setAllFoodItems] = useState([])
+
+  async function fetchAllItems() {
+    const response = await axios.get('/allFoodItems')
+    setAllFoodItems(response.data.data)
+  }
+
+  async function fetchSpecificItems() {
+    const response = await axios.get(`/foodItems?title=${searchText}`)
+    setAllFoodItems(response.data.data)
+  }
+  useEffect(() => {
+    if (searchText.length > 0) {
+      fetchSpecificItems()
+    }
+    else {
+      fetchAllItems()
+    }
+  }, [searchText])
+
+  useEffect(() => {
+    loginRequired()
+  }, [])
+
+
   return (
-    <div>FlavorItem</div>
+    <>
+          <div className='search-container text-center mt-5'>
+        <input type="text" placeholder='Search' className='input-search search-box'
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)} />
+      </div>
+
+      <div className='food-items-result'>
+        <div class="row">
+          {
+            currentFoodItems?.map((foodItem, index) => {
+              return (<FoodItemCard description={foodItem.description} category={foodItem.category} title={foodItem.title} price={foodItem.price} imgUrl={foodItem.imgUrl} key={index} />)
+            })
+          }
+        </div>
+      </div>
+    </>
   )
 }
 
--- client/src/view/Home/Home.js
+++ client/src/view/Home/Home.js
@@ -1,37 +1,8 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import FoodItemCard from '../../components/FoodItemCard/FoodItemCard';
-import { loginRequired } from '../../utils/loginRequired';
+
 import homePageImg from './home-page-icecream.jpg'
 import './Home.css'
 
 function Home() {
-
-  const [searchText, setSearchText] = useState("");
-  const [currentFoodItems, setAllFoodItems] = useState([])
-
-  async function fetchAllItems() {
-    const response = await axios.get('/allFoodItems')
-    setAllFoodItems(response.data.data)
-  }
-
-  async function fetchSpecificItems() {
-    const response = await axios.get(`/foodItems?title=${searchText}`)
-    setAllFoodItems(response.data.data)
-  }
-  useEffect(() => {
-    if (searchText.length > 0) {
-      fetchSpecificItems()
-    }
-    else {
-      fetchAllItems()
-    }
-  }, [searchText])
-
-  useEffect(() => {
-    loginRequired()
-  }, [])
-
   return (
     <>
 
@@ -53,22 +24,7 @@ function Home() {
           </div>
         </div>
       </div>
-      
-      <div className='search-container text-center mt-5'>
-        <input type="text" placeholder='Search' className='input-search search-box'
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)} />
-      </div>
-
-      <div className='food-items-result'>
-        <div class="row">
-          {
-            currentFoodItems?.map((foodItem, index) => {
-              return (<FoodItemCard description={foodItem.description} category={foodItem.category} title={foodItem.title} price={foodItem.price} imgUrl={foodItem.imgUrl} key={index} />)
-            })
-          }
-        </div>
-      </div>
+    
     </>
   )
 }
