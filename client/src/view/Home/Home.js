cd0a6093c41194022354d3487efd6e81f344be8f
Vaibhavihole31 <itspinki05@gmail.com>
2023-01-27 21:37:48 +0530

    feat: perfect homepage design

--- client/src/view/Home/Home.js
+++ client/src/view/Home/Home.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import FoodItemCard from '../../components/FoodItemCard/FoodItemCard';
 import { loginRequired } from '../../utils/loginRequired';
-import homePageImg from './homePageImg.jpg'
+import homePageImg from './home-page-icecream.jpg'
 import './Home.css'
 
 function Home() {
@@ -11,16 +11,12 @@ function Home() {
   const [currentFoodItems, setAllFoodItems] = useState([])
 
   async function fetchAllItems() {
-    // console.log('fetching all items')
     const response = await axios.get('/allFoodItems')
-    // console.log(response.data.data)
     setAllFoodItems(response.data.data)
   }
 
   async function fetchSpecificItems() {
-    // console.log('fetching specific items')
     const response = await axios.get(`/foodItems?title=${searchText}`)
-    // console.log(response.data.data)
     setAllFoodItems(response.data.data)
   }
   useEffect(() => {
@@ -42,7 +38,13 @@ function Home() {
       <div className='container'>
         <div className='row'>
           <div className='col-md-6 item'>
-             hello
+             <h1 className='text-center'>Ice cream is perfection!</h1>
+
+             <h4 className='text-center mt-5'>And so are all the delicious recipes that contain it. Start here with the Perfect Affogato!</h4>
+             
+             <div className='text-center mt-5'>
+             <button className="flavor-btn text-center"><b>Try Our Flavor 🍧</b></button>
+             </div>
           </div>
           <div className='col-md-6'>
             <div> 
