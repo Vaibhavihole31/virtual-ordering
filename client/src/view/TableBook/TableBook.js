e0a65662b3e9a42ebb11f04c88691fa6c8d05f14
Vaibhavihole31 <itspinki05@gmail.com>
2023-02-09 11:05:39 +0530

    feat: table card

--- client/src/view/TableBook/TableBook.js
+++ client/src/view/TableBook/TableBook.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import './TableBook.css'
+// import emptyTable from './empty-table.png'
+import emptyTable from './table-with-boy-girl.png'
 
 function TableBook() {
   return (
-    <div>TableBook</div>
+    <>
+    <div className='container container-booktable'>
+      <div className='row'>
+         <div className='table-card'>
+          <img src={emptyTable} />
+          <h3>Table - 101</h3>
+          <button>Book Table</button>
+         </div>
+         <div className='table-card'>
+          <img src={emptyTable} />
+          <h3>Table - 101</h3>
+          <button>Book Table</button>
+         </div>
+         <div className='table-card'>
+          <img src={emptyTable} />
+          <h3>Table - 101</h3>
+          <button>Book Table</button>
+         </div>
+         <div className='table-card'>
+          <img src={emptyTable} />
+          <h3>Table - 101</h3>
+          <button>Book Table</button>
+         </div>
+      </div>
+    </div>
+    </>
   )
 }
 
