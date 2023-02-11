03dd40ad1fb79446ec43790a4c6d7845a728f0ee
Vaibhavihole31 <itspinki05@gmail.com>
2023-02-11 11:51:06 +0530

    feat: featch table in database

--- client/src/view/TableBook/TableBook.js
+++ client/src/view/TableBook/TableBook.js
@@ -1,42 +1,73 @@
-import React from 'react'
-import './TableBook.css'
+import React, { useState, useEffect } from 'react';
+import './TableBook.css';
+import axios from 'axios';
 // import emptyTable from './empty-table.png'
-import emptyTable from './table-with-boy-girl.png'
+import emptyTable from './table-with-boy-girl.png';
+import swal from 'sweetalert';
+import {virtualUser} from './../../utils/virtualUser'
 
 function TableBook() {
+
+  const [availabeTable, setAvailabeTable] = useState([]);
+
+  useEffect(() => {
+    fetchAvailabeTalbles();
+  }, []);
+
+  async function fetchAvailabeTalbles() {
+    console.log('fetching all tables');
+    const response = await axios.get('/avilableTables');
+    console.log(response.data.data);
+    setAvailabeTable(response.data.data);
+  }
+
+  async function bookThisTable(e) {
+    const response = await axios.post('/bookTable', {
+      tableNumber: e.target.value,
+      userId: virtualUser._id,
+    });
+
+    console.log(e.target.value);
+    console.log(virtualUser);
+
+    if (response.data.success) {
+      await swal({
+        title: 'Success',
+        text: 'Table Booked Successfully',
+        icon: 'success',
+        button: 'OK',
+      });
+      window.location.href = '/dashboard';
+      localStorage.setItem('bookedTable', JSON.stringify(response.data.data));
+    } else {
+      await swal({
+        title: 'Error',
+        text: 'Table Already Booked',
+        icon: 'error',
+        button: 'OK',
+      });
+    }
+  }
+
   return (
     <>
     <div className='container container-booktable'>
       <div className='row'>
         <h3 className='text-center'><b>Book Table</b></h3>
-         <div className='table-card'>
+         {
+          availabeTable?.map((table, index) =>{
+            return (
+              <div className='table-card'>
           <img src={emptyTable} />
           <div className='text-center mt-2'>
-            <h3 className='text-center'>Table - 101</h3>
-          <button className='text-center mt-2 btn-book-table'><b>Book Table</b></button>
+            <h3 className='text-center'>Table - {table.tableNumber}</h3>
+          <button className='text-center mt-2 btn-book-table' value={table.tableNumber}
+                  onClick={bookThisTable}><b>Book Table</b></button>
           </div>
          </div>
-         <div className='table-card'>
-          <img src={emptyTable} />
-          <div className='text-center mt-2'>
-            <h3 className='text-center'>Table - 101</h3>
-          <button className='text-center mt-2 btn-book-table'><b>Book Table</b></button>
-          </div>
-         </div>
-         <div className='table-card'>
-          <img src={emptyTable} />
-          <div className='text-center mt-2'>
-            <h3 className='text-center'>Table - 101</h3>
-          <button className='text-center mt-2 btn-book-table'><b>Book Table</b></button>
-          </div>
-         </div>
-         <div className='table-card'>
-          <img src={emptyTable} />
-          <div className='text-center mt-2'>
-            <h3 className='text-center'>Table - 101</h3>
-          <button className='text-center mt-2 btn-book-table'><b>Book Table</b></button>
-          </div>
-         </div>
+            )
+          })
+         }
       </div>
     </div>
     </>
